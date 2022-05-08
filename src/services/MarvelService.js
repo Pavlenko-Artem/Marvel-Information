62685caadce1d296b651a5f3e5abbe427dcca3e4
Artem Pavlenko <artempavlenko1703@gmail.com>
2022-05-08 18:04:32 +0300

    Add function getResources

---
+++ src/services/MarvelService.js
@@ -0,0 +1,13 @@
+
+
+class MarvelService {
+  getResources = async (url) => {
+    const res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+
+    return await res.json();
+  }
+}
