dfcc86da520673be47c37bea3e09c471ab97a80c
Artem Pavlenko <artempavlenko1703@gmail.com>
2022-05-08 19:22:01 +0300

    Add function getCharacter

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -12,7 +12,11 @@ class MarvelService {
   }
 
   getAllCharacters = () => {
-    return this.getResources('https://gateway.marvel.com:443/v1/public/characters?apikey=9b86234cc5a303cbdc10975ed742e691');
+    return this.getResources('https://gateway.marvel.com:443/v1/public/characters?limit=9&offset=210&apikey=9b86234cc5a303cbdc10975ed742e691');
+  }
+
+  getCharacter = (id) => {
+    return this.getResources(`https://gateway.marvel.com:443/v1/public/characters/${id}?apikey=9b86234cc5a303cbdc10975ed742e691`);
   }
 }
 
