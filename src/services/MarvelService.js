7f9a96bca52ed9d6030de2027e27bad03ac2e88b
Artem Pavlenko <artempavlenko1703@gmail.com>
2022-05-19 01:08:01 +0300

    test

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,9 @@
 
 
 class MarvelService {
+  _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+  _apiKey = 'apikey=9b86234cc5a303cbdc10975ed742e691';
+
   getResources = async (url) => {
     const res = await fetch(url);
 
@@ -11,8 +14,24 @@ class MarvelService {
     return await res.json();
   }
 
-  getAllCharacters = () => {
-    return this.getResources('https://gateway.marvel.com:443/v1/public/characters?apikey=9b86234cc5a303cbdc10975ed742e691');
+  getAllCharacters = async () => {
+    const res = await this.getResources(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    return res.data.results.map(this._transformCharacter);
+  }
+
+  getCharacter = async (id) => {
+    const res = await this.getResources(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    return this._transformCharacter(res.data.results[0]);
+  }
+
+  _transformCharacter = (char) => {
+    return {
+      name: char.name,
+      description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+      thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url
+    }
   }
 }
 
