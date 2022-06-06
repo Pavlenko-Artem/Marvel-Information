2f14e89868e3255fec9f358d15de3120e0395b2e
Artem Pavlenko <artempavlenko1703@gmail.com>
2022-06-06 18:51:01 +0300

    Add variables _baseOffset

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -3,6 +3,7 @@
 class MarvelService {
   _apiBase = 'https://gateway.marvel.com:443/v1/public/';
   _apiKey = 'apikey=9b86234cc5a303cbdc10975ed742e691';
+  _baseOffset = 210;
 
   getResources = async (url) => {
     const res = await fetch(url);
@@ -14,8 +15,8 @@ class MarvelService {
     return await res.json();
   }
 
-  getAllCharacters = async () => {
-    const res = await this.getResources(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+  getAllCharacters = async (offset = this._baseOffset) => {
+    const res = await this.getResources(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
     return res.data.results.map(this._transformCharacter);
   }
 
